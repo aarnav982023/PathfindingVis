a830149ef3c86ff3014e7f5438bf006afb354e71
aarnav982023 <aarnav982023@gmail.com>
2019-12-27 02:18:01 +0530

    Updated Jump Point Search

--- src/algorithms/jumpPointSearch.js
+++ src/algorithms/jumpPointSearch.js
@@ -39,125 +39,161 @@ const jumpPointSearch = (grid, startNode, endNode) => {
   });
   while (pq.length) {
     const obj = pq.dequeue();
-    /*if (!obj.node.isVisited) {
+    if (!obj.node.isVisited) {
       obj.node.isVisited = true;
       visitedNodes.push(obj.node);
-    }*/
-    visitedNodes.push(obj.node);
+    }
+    console.log(
+      obj.node.row +
+        "," +
+        obj.node.col +
+        "-dir:" +
+        obj.dir[0] +
+        "," +
+        obj.dir[1] +
+        "-f:" +
+        obj.node.f
+    );
     const response = scan(obj.node, obj.dir, grid, endNode, pq);
-    console.log(response);
     if (response === "found") break;
   }
   shortestPath = getShortestPath(grid[endNode.row][endNode.column]);
   return { visitedNodes, shortestPath };
 };
-
+//x = c // y = r
+//hor = y // ver = x
 const scan = (node, dir, grid, endNode, pq) => {
   const x = dir[0];
   const y = dir[1];
-  const r = node.row;
-  const c = node.col;
-  let r1 = r + x;
-  let c1 = c + y;
-  if (!inGrid(r1, c1, grid)) return false;
-  if (r1 === endNode.row && c1 === endNode.column) return "found";
-  if (grid[r1][c1].isWall) return false;
-  grid[r1][c1].g = grid[r][c].g + 1;
-  grid[r1][c1].f = grid[r1][c1].g + H(r1, c1, endNode);
-  grid[r1][c1].prevNode = grid[r][c];
   if (Math.abs(x) === 1 && Math.abs(y) === 1) {
+    let r0 = node.row;
+    let c0 = node.col;
     while (true) {
-      if (r1 === endNode.row && c1 === endNode.column) return "found";
-      if (grid[r1][c1].isWall) return false;
-      const h = scan(grid[r1][c1], [0, y], grid, endNode, pq);
-      const v = scan(grid[r1][c1], [x, 0], grid, endNode, pq);
-      if (h === "found" || v === "found") return "found";
-      if (h || v) {
+      //console.log("diag");
+      //console.log(r0, c0);
+      let c1 = c0 + y;
+      let r1 = r0 + x;
+      if (!inGrid(r1, c1, grid)) return false;
+      let g = grid[r1][c1];
+      grid[r1][c1].g = grid[r0][c0].g + 1;
+      grid[r1][c1].f = grid[r1][c1].g + H(r1, c1, endNode);
+      grid[r1][c1].prevNode = grid[r0][c0];
+      if (g.row === endNode.row && g.col === endNode.column) return "found";
+      if (g.isWall) return false;
+      let c2 = c1 + y;
+      let r2 = r1 + x;
+      let jump = false;
+      if (
+        inGrid(r1, c0, grid) &&
+        grid[r1][c0].isWall &&
+        inGrid(r2, c0, grid) &&
+        !grid[r2][c0].isWall
+      ) {
+        pq.queue({ node: grid[r1][c1], dir: [x, -y] });
+        jump = true;
+      }
+      if (
+        inGrid(r0, c1, grid) &&
+        grid[r0][c1].isWall &&
+        inGrid(r0, c2, grid) &&
+        !grid[r0][c2].isWall
+      ) {
+        pq.queue({ node: grid[r1][c1], dir: [-x, y] });
+        jump = true;
+      }
+      let hor = scan(grid[r1][c1], [0, y], grid, endNode, pq);
+      let ver = scan(grid[r1][c1], [x, 0], grid, endNode, pq);
+      if (hor === "found" || ver === "found") return "found";
+      if (hor || ver) {
         pq.queue({ node: grid[r1][c1], dir: [x, y] });
         return true;
       }
-      if (inGrid(r1 + x, c1 + y, grid)) {
-        grid[r1 + x][c1 + y].g = grid[r1][c1].g + 1;
-        grid[r1 + x][c1 + y].f =
-          grid[r1 + x][c1 + y].g + H(r1 + x, c1 + y, endNode);
-        grid[r1 + x][c1 + y].prevNode = grid[r1][c1];
-        r1 += x;
-        c1 += y;
-      } else return false;
+      if (jump) {
+        pq.queue({ node: grid[r1][c1], dir: [x, y] });
+        return true;
+      }
+      c0 = c1;
+      r0 = r1;
     }
   } else if (x === 0) {
+    let r0 = node.row;
+    let c0 = node.col;
     while (true) {
-      if (r1 === endNode.row && c1 === endNode.column) return "found";
-      if (grid[r1][c1].isWall) return false;
+      //console.log("row");
+      //console.log(r0, c0);
+      let c1 = c0 + y;
+      if (!inGrid(r0, c1, grid)) return false;
+      let g = grid[r0][c1];
+      grid[r0][c1].g = grid[r0][c0].g + 1;
+      grid[r0][c1].f = grid[r0][c1].g + H(r0, c1, endNode);
+      grid[r0][c1].prevNode = grid[r0][c0];
+      if (g.row === endNode.row && g.col === endNode.column) return "found";
+      if (g.isWall) return false;
+      let c2 = c1 + y;
       let jump = false;
-      if (r1 === endNode.row && c1 === endNode.column) return "found";
       if (
-        inGrid(r1 + 1, c1, grid) &&
-        grid[r1 + 1][c1].isWall &&
-        inGrid(r1 + 1, c1 + y, grid) &&
-        !grid[r1 + 1][c1 + y].isWall
+        inGrid(r0 - 1, c1, grid) &&
+        grid[r0 - 1][c1].isWall &&
+        inGrid(r0 - 1, c2, grid) &&
+        !grid[r0 - 1][c2].isWall
       ) {
-        pq.queue({ node: grid[r1][c1], dir: [1, y] });
+        pq.queue({ node: grid[r0][c1], dir: [-1, y] });
         jump = true;
       }
       if (
-        inGrid(r1 - 1, c1, grid) &&
-        grid[r1 - 1][c1].isWall &&
-        inGrid(r1 - 1, c1 + y, grid) &&
-        !grid[r1 - 1][c1 + y].isWall
+        inGrid(r0 + 1, c1, grid) &&
+        grid[r0 + 1][c1].isWall &&
+        inGrid(r0 + 1, c2, grid) &&
+        !grid[r0 + 1][c2].isWall
       ) {
-        pq.queue({ node: grid[r1][c1], dir: [-1, y] });
+        pq.queue({ node: grid[r0][c1], dir: [1, y] });
         jump = true;
       }
       if (jump) {
-        pq.queue({ node: grid[r1][c1], dir: [x, y] });
+        pq.queue({ node: grid[r0][c1], dir: [0, y] });
         return true;
       }
-      if (inGrid(r1 + x, c1 + y, grid)) {
-        grid[r1 + x][c1 + y].g = grid[r1][c1].g + 1;
-        grid[r1 + x][c1 + y].f =
-          grid[r1 + x][c1 + y].g + H(r1 + x, c1 + y, endNode);
-        grid[r1 + x][c1 + y].prevNode = grid[r1][c1];
-        r1 += x;
-        c1 += y;
-      } else return false;
+      c0 = c1;
     }
   } else if (y === 0) {
+    let r0 = node.row;
+    let c0 = node.col;
     while (true) {
-      if (r1 === endNode.row && c1 === endNode.column) return "found";
-      if (grid[r1][c1].isWall) return false;
+      //console.log("col");
+      //console.log(r0, c0);
+      let r1 = r0 + x;
+      if (!inGrid(r1, c0, grid)) return false;
+      let g = grid[r1][c0];
+      grid[r1][c0].g = grid[r0][c0].g + 1;
+      grid[r1][c0].f = grid[r1][c0].g + H(r1, c0, endNode);
+      grid[r1][c0].prevNode = grid[r0][c0];
+      if (g.row === endNode.row && g.col === endNode.column) return "found";
+      if (g.isWall) return false;
+      let r2 = r1 + x;
       let jump = false;
-      if (r1 === endNode.row && c1 === endNode.column) return "found";
       if (
-        inGrid(r1, c1 + 1, grid) &&
-        grid[r1][c1 + 1].isWall &&
-        inGrid(r1 + x, c1 + 1, grid) &&
-        !grid[r1 + x][c1 + 1].isWall
+        inGrid(r1, c0 - 1, grid) &&
+        grid[r1][c0 - 1].isWall &&
+        inGrid(r2, c0 - 1, grid) &&
+        !grid[r2][c0 - 1].isWall
       ) {
-        pq.queue({ node: grid[r1][c1], dir: [x, 1] });
+        pq.queue({ node: grid[r1][c0], dir: [x, -1] });
         jump = true;
       }
       if (
-        inGrid(r1, c1 - 1, grid) &&
-        grid[r1][c1 - 1].isWall &&
-        inGrid(r1 + x, c1 - 1, grid) &&
-        !grid[r1 + x][c1 - 1].isWall
+        inGrid(r1, c0 + 1, grid) &&
+        grid[r1][c0 + 1].isWall &&
+        inGrid(r2, c0 + 1, grid) &&
+        !grid[r2][c0 + 1].isWall
       ) {
-        pq.queue({ node: grid[r1][c1], dir: [x, -1] });
+        pq.queue({ node: grid[r1][c0], dir: [x, 1] });
         jump = true;
       }
       if (jump) {
-        pq.queue({ node: grid[r1][c1], dir: [x, y] });
+        pq.queue({ node: grid[r1][c0], dir: [x, 0] });
         return true;
       }
-      if (inGrid(r1 + x, c1 + y, grid)) {
-        grid[r1 + x][c1 + y].g = grid[r1][c1].g + 1;
-        grid[r1 + x][c1 + y].f =
-          grid[r1 + x][c1 + y].g + H(r1 + x, c1 + y, endNode);
-        grid[r1 + x][c1 + y].prevNode = grid[r1][c1];
-        r1 += x;
-        c1 += y;
-      } else return false;
+      r0 = r1;
     }
   }
 };
@@ -170,8 +206,7 @@ const H = (row, col, endNode) => {
   const dx = Math.abs(row - endNode.row);
   const dy = Math.abs(col - endNode.column);
   const d = 1;
-  let ans;
-  ans = d * (dx + dy);
+  let ans = d * Math.sqrt(dx * dx + dy * dy);
   /*if (heuristic === "manhatten") {
       ans = d * (dx + dy);
     }
